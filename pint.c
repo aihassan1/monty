43255acc28d307c5e8d83f8cb9b7fd72454f285c
aihassan1 <abdosruor57@gmail.com>
2023-10-17 16:52:08 +0000

    pint.c

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+/**
+* pint - Print the value at the top of the stack.
+* @stack: A pointer to the stack.
+* @line_number: The line number of the instruction.
+*/
+void pint(stack_t **stack, unsigned int line_number)
+{
+int value;
+
+if (*stack == NULL)
+{
+fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+value = (*stack)->n;
+printf("%d\n", value);
+}
