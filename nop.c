401411363c5ea5720a71e0d47b2f663bbcfca8e1
root <root@DESKTOP-GTOBK44>
2023-10-18 22:31:57 +0100

    add

--- nop.c
+++ nop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
  * nop - function that do nothing
- * @param stack: stack
- * @param line_number: line number
+ * @stack: stack
+ * @line_number: line number
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
 	(void)stack;
+	(void)line_number;
 }
