dcfbf1d00f3dbb7ae554c5f4c87bf77c42808fa1
root <root@DESKTOP-GTOBK44>
2023-10-19 09:26:27 +0100

    add-_-

--- mdm.c
+++ mdm.c
@@ -8,23 +8,23 @@
 
 void mood(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp = *stack;
-        int result;
+	stack_t *temp = *stack;
+	int result;
 
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        if ((*stack)->n == 0)
-        {
-                fprintf(stderr, "L%d: division by zero\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        result = (*stack)->next->n % (*stack)->n;
-        (*stack)->next->n = result;
-        (*stack) = (*stack)->next;
-        free(temp);
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = (*stack)->next->n % (*stack)->n;
+	(*stack)->next->n = result;
+	(*stack) = (*stack)->next;
+	free(temp);
 }
 
 /**
